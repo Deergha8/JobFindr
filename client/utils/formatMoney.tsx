abb50d3ea009e60e716de7b883d658e0ede85cf8
Deergha Srivastava <deerghasrivastava@gmail.com>
2025-06-23 00:23:08 +0530

    build #1

--- client/utils/formatMoney.tsx
+++ client/utils/formatMoney.tsx
@@ -1,7 +1,7 @@
 import currency from "currency.js";
 
 const formatMoney = (amount: number, currencyCode: string) => {
-  const symbol = currencyCode === "GBP" ? "£" : "$";
+  const symbol = currencyCode === "INR" ? "₹" : "$";
 
   return currency(amount, {
     symbol,
